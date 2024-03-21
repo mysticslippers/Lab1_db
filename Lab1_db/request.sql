34349bdd3080cf436bc54f741354013ca5486d06
Mikhailov Dmitry <eternal.martyr13579@gmail.com>
2024-03-21 12:34:43 +0300

    delete Lab1_db directory

--- Lab1_db/request.sql
+++
@@ -1,4 +0,0 @@
----Additional task with select request
-SELECT account.id, account.total, account.client_id, client.name
-    FROM account INNER JOIN client ON account.client_id = client.id
-        INNER JOIN branch ON (account.branch_id = branch.id) AND (branch.name = 'НСВ')
